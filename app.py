265b3552ab9288ded7316ded4a48874604430de6
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2016-07-05 19:01:34 +0900

    fix a bug

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import os
 from flask import Flask, render_template, redirect, url_for
-from flask.ext.script import Manager
 
 from config import Config
 
 app = Flask(__name__)
-
-manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -15,21 +12,21 @@ def index():
 
 @app.route('/stop')
 def do_stopping():
-    os.system('irsend SEND_ONCE {0} {1}'.format(Config.CONTROLLER_NAME, Config.SIGNALS['stop'])
+    os.system('irsend SEND_ONCE {0} {1}'.format(Config.CONTROLLER_NAME, Config.SIGNALS['stop']))
     state = 'Now Stopping'
     return render_template('index.html', state=state)
 
 @app.route('/warm')
 def do_warming():
-    os.system('irsend SEND_ONCE {0} {1}'.format(Config.CONTROLLER_NAME, Config.SIGNALS['warm'])
+    os.system('irsend SEND_ONCE {0} {1}'.format(Config.CONTROLLER_NAME, Config.SIGNALS['warm']))
     state = 'Now Warming'
     return render_template('index.html', state=state)
 
 @app.route('/cool')
 def do_cooling():
-    os.system('irsend SEND_ONCE {0} {1}'.format(Config.CONTROLLER_NAME, Config.SIGNALS['cool'])
+    os.system('irsend SEND_ONCE {0} {1}'.format(Config.CONTROLLER_NAME, Config.SIGNALS['cool']))
     state = 'Now Cooling'
     return render_template('index.html', state=state)
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
