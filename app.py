d15f38bde0bf0aba99157a506cb2609bd70832b2
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2016-07-06 15:19:10 +0900

    complete operation display temprarily

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def do_cooling():
     return render_template('index.html', state=state)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5050)
+    app.run(debug=True, host="0.0.0.0", port=5055)
